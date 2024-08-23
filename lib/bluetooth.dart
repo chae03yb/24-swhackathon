4972a413b92d50d702087fa1b6c34f271cce567a
Chae <chae03yb@gmail.com>
2024-08-23 22:42:21 +0900

    added some methods: bluetooth.dart

--- lib/bluetooth.dart
+++ lib/bluetooth.dart
@@ -1,29 +1,64 @@
+import "dart:typed_data";
+
 import "package:bluetooth_classic/bluetooth_classic.dart";
 
+class DeviceNotPairedError extends Error {}
 
 class BluetoothHelper {
   BluetoothHelper._();
 
   static final BluetoothHelper instance = BluetoothHelper._();
-  BluetoothClassic bluetoothClassicInstance;
+  BluetoothClassic bluetoothClassicInstance = BluetoothClassic();
+  bool connected = false;
+  Uint8List rawData = Uint8List(0);
 
   factory BluetoothHelper() {
     return instance;
   }
 
   void init() {
-    bluetoothClassicInstance = BluetoothClassic();
+    bluetoothClassicInstance.initPermissions();
+  }
+
+  void connect(String address, String serviceUUID) async {
+    connected = await bluetoothClassicInstance.connect(address, serviceUUID);
+  }
+
+  void disconnect() async {
+    if (!connected) {
+      throw DeviceNotPairedError();
+    }
+
+    connected = await bluetoothClassicInstance.disconnect();
   }
 
   void getRecordFile() async {
+    if (!connected) {
+      throw DeviceNotPairedError();
+    }
 
+    await bluetoothClassicInstance.write("PULL REC");
   }
 
   void getBrakeRecordFile() async {
+    if (!connected) {
+      throw DeviceNotPairedError();
+    }
 
+    await bluetoothClassicInstance.write("PULL BRK");
   }
 
   void getRPMRecordFile() async {
+    if (!connected) {
+      throw DeviceNotPairedError();
+    }
 
+    await bluetoothClassicInstance.write("PULL RPM");
+  }
+
+  void onDataReceivedHandler(Function f) {
+    bluetoothClassicInstance.onDeviceDataReceived().listen((event) {
+      rawData = Uint8List.fromList([...rawData, ...event]);
+    });
   }
 }
