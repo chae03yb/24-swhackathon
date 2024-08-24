eaac3ae000a36fe4f3cc1f72e1eeecc3720880bf
gjrjsghl <aa01021107861@gmail.com>
2024-08-25 04:06:58 +0900

    끝

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:pick_or_save/pick_or_save.dart';
+import 'package:video_player/video_player.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 
 class Mainpage extends StatefulWidget {
@@ -13,9 +18,9 @@ class Mainpage extends StatefulWidget {
 
 int _selected = 0;
 
-List<String> titleText = ['블랙박스 기록', '가감속 기록', '회전수 기록'];
+List<String> titleText = ['블랙박스 기록', '페달 기록', 'RPM 기록'];
 List<Widget> _selectedTop = [
-  blackbox(), gagamsok(), hoejeonsu()
+  blackbox(), DrawData(kind: 0), DrawData(kind: 1)
 ];
 List<String> saveName = ["blackbox.mp3","gagamsok.txt","hoejeonsu.txt"];
 
@@ -24,11 +29,6 @@ class _MainpageState extends State<Mainpage> {
   @override
   void initState()  {
     super.initState();
-
-    getApplicationDocumentsDirectory().then((value) {
-      for(int i = 0;i<3;i++)
-        File(value.path + '/' + saveName[i]).openWrite(mode: FileMode.append).write("");
-    });
   }
 
   @override
@@ -40,6 +40,7 @@ class _MainpageState extends State<Mainpage> {
         title : Text(titleText[_selected],style: TextStyle(fontSize: 30),),
         toolbarHeight: 60,
         centerTitle: true,
+        backgroundColor: Color.fromARGB(150, 0x02, 0x7D, 0xFD),
       ),
 
       body: Container(
@@ -47,13 +48,21 @@ class _MainpageState extends State<Mainpage> {
         width: screenWidth,
 
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
               height: (screenHeight-45)/2,
               width: screenWidth,
               child: Stack(
                 children: [
-                  _selectedTop[_selected],
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(15),
+                      width: screenWidth,
+                      height: (screenHeight-45)/2,
+                      child: _selectedTop[_selected],
+                    ),
+                  ),
                   Column(
                     children: [
                       SizedBox(height: (screenHeight-45)/2-60,width: screenWidth),
@@ -78,25 +87,26 @@ class _MainpageState extends State<Mainpage> {
               )
             ),
 
-            Divider(thickness: 1,color: Colors.black,indent: 15,endIndent: 15,),
 
             Expanded(
               child: Container(
                 height: (screenHeight-45)/2,
                 width: screenWidth,
-
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(239, 190, 111,1),
+                  borderRadius: BorderRadius.only(topLeft : Radius.circular(40), topRight: Radius.circular(40)),
+                ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-
                     for(int i = 0;i<3;i++) GestureDetector(
                       child: Container(
                         height: (screenHeight-45)/2/6.5,
                         width: screenWidth-50,
-                        child: Text(titleText[i],style: TextStyle(fontSize: 20),),
+                        child: Text(titleText[i],style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
                         decoration: BoxDecoration(
-                          color: Color.fromARGB(199, 175, 175, 175),
+                          color: Color.fromARGB(50, 200, 200, 200),
                           borderRadius: BorderRadius.all(Radius.circular(15)),
                         ),
                         padding: EdgeInsets.only(left: 20),
@@ -105,10 +115,10 @@ class _MainpageState extends State<Mainpage> {
                       onTap: () {
                         setState(() {
                           _selected = i;
+                          selecteditem = -1;
                         });
                       },
                     ),
-
 
                   ],
                 )
@@ -131,51 +141,157 @@ class blackbox extends StatefulWidget {
 }
 
 class _blackboxState extends State<blackbox> {
+  late VideoPlayerController _controller;
   @override
-  Widget build(BuildContext context) {
-    return Container(color: Colors.yellow);
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = VideoPlayerController.asset("assets/bb.mp4")
+      ..initialize();
+    _controller.play();
+    _controller.setLooping(true);
   }
-}
-
-
-//가감속 기록 창
-class gagamsok extends StatelessWidget {
-  const gagamsok({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
-  }
-}
-
-//회전수 기록 창
-class hoejeonsu extends StatelessWidget {
-  const hoejeonsu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green);
+    return VideoPlayer(_controller);
   }
 }
 
-// 파일 쓰기
-void writeFile(String s) async {
-  // 내부 저장소의 경로를 가져올 때 getTemporaryDirectory() 함수를 이용할 수 있으나, 임시 디렉터리는 캐시를 이용하므로 앱이 종료되고 일정 시간이 지나면 사라질 수 있다.
-  var dir = await getApplicationDocumentsDirectory();
-  File(dir.path + '/' + saveName[_selected]).openWrite(mode: FileMode.append).write(s+"/n");
+
+
+
+class DrawData extends StatefulWidget {
+  int kind;
+  DrawData({required this.kind});
+
+  @override
+  State<DrawData> createState() => _DrawDataState();
 }
 
-// 파일 읽기
-void readFile() async {
-  try {
-    var dir = await getApplicationDocumentsDirectory();
-    var file = await File(dir.path + '/'+saveName[_selected]).readAsString();
-    print(file);
-  } catch (e) {
-    print(e.toString());
+int selecteditem = -1;
+class _DrawDataState extends State<DrawData> {
+  String url = '192.168.4.1';
+  var itemcnt;
+  var gotres;
+  List<String> getKeyword = ['/brake','/rpm'];
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    if(selecteditem == -1) {
+      return FutureBuilder(
+          future: getList(),
+          builder: (BuildContext cxt, AsyncSnapshot snapshot) {
+            debugPrint("futurebuilder !!!");
+            debugPrint("data is : ${snapshot.data.toString()}");
+            if(snapshot.hasData == false) {
+              return CircularProgressIndicator();
+            }
+            else if (snapshot.hasError) {
+              return Center(child :Text(snapshot.error.toString()));
+            }
+            else {
+              debugPrint("sadfqsdfasdfads");
+
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext ct, int idx) {
+                  return GestureDetector(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      child: Text(snapshot.data[idx].toString()),
+                    ),
+                    onTap: () {
+                      setState(() {
+                        selecteditem = idx;
+                      });
+                    },
+                  );
+                },
+              );
+            }
+        }
+      );
+    }
+    
+    else {
+      return FutureBuilder(
+        future: readFile(),
+        builder: (BuildContext cxt, AsyncSnapshot snapshot) {
+          if(snapshot.hasData == false) {
+            return CircularProgressIndicator();
+          }
+          else if (snapshot.hasError) {
+            return Center(child :Text(snapshot.error.toString()));
+          }
+          else {
+            return Container(
+              child: Text(snapshot.data),
+            );
+          }
+        }
+    );
+    }
+  }
+
+  Future<dynamic> getList() async {
+    debugPrint("start get List()");
+    final queryUrl = Uri.parse("http://$url${getKeyword[widget.kind]}/list");
+    debugPrint("!!!!!!$queryUrl");
+
+    final res = await http.get(queryUrl);
+    debugPrint("Result: ${res.body}");
+
+    if (res.statusCode == 200) {
+      debugPrint("gooooooooood connect!!!!!!!!!!!!");
+      gotres = convert.jsonDecode(res.body) as dynamic;
+      debugPrint("aaaaaa   ${gotres.runtimeType} ${gotres}");
+      debugPrint(gotres.toString());
+      return gotres;
+    }
+    else {
+      debugPrint("error on getList");
+      return ['error'];
+    }
+  }
+
+
+
+
+  Future<String> readFile() async {
+
+
+    var file = "";
+    debugPrint("now valus is ${gotres[selecteditem]} !!!!!!!!!!!!!");
+    var filename = gotres[selecteditem].toString().split("/")[3].substring(0,8);
+
+    debugPrint("now valus is ${filename} !!!!!!!!!!!!!");
+    final queryurl = "http://"+url+getKeyword[widget.kind]+"/get?filename="+filename;
+    debugPrint("!!!!!!!!! ${queryurl.toString()}");
+    var res = await http.get(Uri.parse(queryurl));
+    if(res.statusCode == 200) {
+      debugPrint(res.body.toString());
+      var dir = await getApplicationDocumentsDirectory();
+      File(dir.path+'/'+saveName[_selected]).writeAsStringSync(res.body);
+
+      try {
+        file = String.fromCharCodes(File('${dir.path}/${saveName[_selected]}').readAsLinesSync().map((e) => int.parse(e)).toList());
+        debugPrint(file);
+      } catch (e) {
+        debugPrint(e.toString());
+      }
+    }
+    else {
+      debugPrint("something goes wrong");
+    }
+    return file;
   }
 }
-
 
 void Outsave() async{
 
